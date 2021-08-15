70551205edce5c6736fc25fac768670a0f057e8f
Maytrok <bluna.el@gmx.de>
2021-08-15 21:50:33 +0200

    BaseApi Request

---
+++ app/Http/Requests/BaseApiRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Overriding this method prevents redirect
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw (new ValidationException($validator));
+    }
+}
