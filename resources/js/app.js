b38db471d2b0f112f0fec021a6aa015d4ba866af
Maytrok <bluna.el@gmx.de>
2021-08-30 13:03:17 +0200

    Frontend Init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+window.Vue = require('vue').default;
+import vuetify from './vuetify';
+window.Vue.component('app', require('./components/App.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    vuetify
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- <router-view></router-view> -->
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Navigation from "./Navigation.vue";
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
+
+<style>
+</style>
